cb3870e43576d2c5be00395c66de5ca8761ddd84
Morayg <moraygg@gmail.com>
2017-09-26 15:40:21 +0300

    master

--- content.js
+++ content.js
@@ -1,9 +1,13 @@
 //получает адрес сайта
 var site = window.location.href;
-var start = performance.now();
+var start = window.performance.timing.domContentLoadedEventEnd;
 
 window.onload = function() {
 	var time = performance.now();
 	time = time - start;
     chrome.runtime.sendMessage({site1:site,time1:time});
+    //выводим алерт если время ожидания превысили 10s
+    if (time > 10000) {
+    	alert ('Time loading page^<br>' + site + '<br>is: ' + (time/1000) + 'sec');
+    }
   };
--- popup.js
+++ popup.js
@@ -4,7 +4,7 @@ var Time = localStorage['time'].split(';');
 var data = '';
 var n = 1;
 //выводит текст в окне
-for(var i = (Page.length - 10); i < Page.length; i++) {
+for(var i = (Page.length - 1); i > Page.length-11; i--) {
 	if(i >= 0) {
 		data = data + '<tr class="timetable__tbody-row"><td class="timetable__id-cell">' + n + '</td><td class="timetable__url-cell">' + Page[i] + '</td><td class="timetable__time-cell"><p class="timetable__time">' + (Time[i] / 1000) + '</p></td></tr>';
 		n++;
